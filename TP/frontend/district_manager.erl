69bea4312e38a92865280a28a99fb752cea1e48f
Hugo Cardoso <48107335+Abjiri@users.noreply.github.com>
2021-01-13 18:30:17 +0000

    sockets dos distritais

--- TP/frontend/district_manager.erl
+++ TP/frontend/district_manager.erl
@@ -4,24 +4,24 @@
 
 start() -> 
     Districts = #{
-        aveiro => {8100, #{}},
-        beja => {8101, #{}},
-        braga => {8102, #{}},
-        bragança => {8103, #{}},
-        castelo_branco => {8104, #{}},
-        coimbra => {8105, #{}},
-        evora => {8106, #{}},
-        faro => {8107, #{}},
-        guarda => {8108, #{}},
-        leiria => {8109, #{}},
-        lisboa => {8110, #{}},
-        portalegre => {8111, #{}},
-        porto => {8112, #{}},
-        santarem => {8113, #{}},
-        setubal => {8114, #{}},
-        viana_do_castelo => {8115, #{}},
-        vila_real => {8116, #{}},
-        viseu => {8117, #{}}
+        aveiro => {element(2,gen_tcp:listen(8100, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        beja => {element(2,gen_tcp:listen(8101, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        braga => {element(2,gen_tcp:listen(8102, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        bragança => {element(2,gen_tcp:listen(8103, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        castelo_branco => {element(2,gen_tcp:listen(8104, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        coimbra => {element(2,gen_tcp:listen(8105, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        evora => {element(2,gen_tcp:listen(8106, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        faro => {element(2,gen_tcp:listen(8107, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        guarda => {element(2,gen_tcp:listen(8108, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        leiria => {element(2,gen_tcp:listen(8109, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        lisboa => {element(2,gen_tcp:listen(8110, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        portalegre => {element(2,gen_tcp:listen(8111, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        porto => {element(2,gen_tcp:listen(8112, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        santarem => {element(2,gen_tcp:listen(8113, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        setubal => {element(2,gen_tcp:listen(8114, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        viana_do_castelo => {element(2,gen_tcp:listen(8115, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        vila_real => {element(2,gen_tcp:listen(8116, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}},
+        viseu => {element(2,gen_tcp:listen(8117, [binary, {active, once}, {packet, 0}, {reuseaddr, true}])), #{}}
     },
     io:fwrite("Districts list: ~p\n", [Districts]),
     register(?MODULE, spawn(fun() -> loop(Districts) end)).
@@ -48,28 +48,28 @@ loop(Districts) ->
             end,
             loop(Districts);
 
-        {register_user, Socket, District, Username, Port} ->
-            {DistPort,Users} = maps:get(District,Districts),
+        {register_user, CliSocket, District, Username, Port} ->
+            {DistSocket,Users} = maps:get(District,Districts),
             NewUsers = maps:put(Username,Port,Users),
             io:fwrite("Registered private notification socket. ~p ~p\n", [Username, Port]),
-            response_manager:sendResponse(Socket,true,"Registered private notification socket."),
-            loop(maps:update(District,{DistPort,NewUsers},Districts));
+            response_manager:sendResponse(CliSocket,true,"Registered private notification socket."),
+            loop(maps:update(District,{DistSocket,NewUsers},Districts));
         
         {location, District, Username, Location} ->
             io:fwrite("D ~p U ~p L ~p\n", [District,Username,Location]),
-            {Port,_} = maps:get(District,Districts),
-            response_manager:sendUserLocation(Port,Username,Location),
+            {Socket,_} = maps:get(District,Districts),
+            response_manager:sendUserLocation(Socket,Username,Location),
             io:fwrite("Sent new location to district server. ~p ~p\n", [Username, District]),
             loop(Districts);
 
         {sick_user, District, Username} ->
-            {Port,_} = maps:get(District,Districts),
-            response_manager:sendSickPing(Port,Username),
+            {Socket,_} = maps:get(District,Districts),
+            response_manager:sendSickPing(Socket,Username),
             loop(Districts);
 
         {{nr_people, Socket, District, Location}, From} ->
-            {Port,_} = maps:get(District,Districts),
-            response_manager:sendLocationToCountPeople(Port,Location),
+            {Socket,_} = maps:get(District,Districts),
+            response_manager:sendLocationToCountPeople(Socket,Location),
             receive
                 {tcp, Socket, Bin} ->
                     inet:setopts(Socket, [{active, once}]),
@@ -82,7 +82,7 @@ loop(Districts) ->
             loop(Districts);
 
         {remove_user, District, Username} ->
-            {DistPort,Users} = maps:get(District,Districts),
+            {DistSocket,Users} = maps:get(District,Districts),
             NewUsers = maps:remove(Username,Users),
-            loop(maps:update(District,{DistPort,NewUsers},Districts))
+            loop(maps:update(District,{DistSocket,NewUsers},Districts))
     end.
