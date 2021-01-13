8d93ca49559466e6d3f3574c3c5709d78b514203
Hugo Cardoso <48107335+Abjiri@users.noreply.github.com>
2021-01-13 17:12:49 +0000

    e4sdsdff

--- TP/frontend/server.erl
+++ TP/frontend/server.erl
@@ -1,12 +1,14 @@
 -module(server).
--export ([start_server/1]).
+-export ([start_server/2]).
 
 % começa a correr o servidor da frontend
-start_server(Port) ->
+start_server(Port1,Port2) ->
     account_manager:start(),
     district_manager:start(),
-    {ok, LSock} = gen_tcp:listen(Port, [binary, {active, once}, {packet, 0}, {reuseaddr, true}]),
+    {ok, LSock} = gen_tcp:listen(Port1, [binary, {active, once}, {packet, 0}, {reuseaddr, true}]),
+    {ok, LSock2} = gen_tcp:listen(Port2, [binary, {active, once}, {packet, 0}, {reuseaddr, true}]),
     spawn(fun() -> acceptor(LSock) end),
+    spawn(fun() -> notif_acceptor(LSock2) end),
     ok.
 
 % processo em loop a aceitar novas conexões, cria um processo por cada socket (cliente) que se conecta
@@ -16,3 +18,11 @@ acceptor(LSock) ->
     spawn(fun() -> acceptor(LSock) end),
     gen_tcp:controlling_process(Sock, self()),
     authenticator:authentication(Sock).
+
+% processo em loop a aceitar novas conexões, cria um processo por cada socket (cliente) que se conecta
+notif_acceptor(LSock) ->
+    {ok, Sock} = gen_tcp:accept(LSock),
+    io:fwrite("Connected socket ~p.\n", [Sock]),
+    spawn(fun() -> notif_acceptor(LSock) end),
+    gen_tcp:controlling_process(Sock, self()),
+    priv_notification_manager:loop(Sock).
